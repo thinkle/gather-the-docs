4c2a5f00e79584705a845e42c38a50fc847e7c3c
Tom Hinkle <tmhinkle@gmail.com>
2023-11-13 10:30:38 -0500

    ++picker stuff

--- src/svelte/clientApi.ts
+++ src/svelte/clientApi.ts
@@ -1,132 +1,138 @@
+
 export const GoogleAppsScript = {
-  getFolderInfo(folderId: string): Promise<import("../gas/copier").Folder> {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler((result: import("../gas/copier").Folder) =>
-          resolve(result)
-        )
-        .withFailureHandler((error: any) => reject(error))
-        .getFolderInfo(folderId);
-    });
-  },
+  
+     getOAuthToken(): Promise<string> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: string) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getOAuthToken();
+      });
+    },
 
-  harvestLinksFromActivePresentation(): Promise<
-    import("../gas/util/links").DriveLink[]
-  > {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler((result: import("../gas/util/links").DriveLink[]) =>
-          resolve(result)
-        )
-        .withFailureHandler((error: any) => reject(error))
-        .harvestLinksFromActivePresentation();
-    });
-  },
+     getApiKey(): Promise<string> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: string) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getApiKey();
+      });
+    },
 
-  copyLinksInPresentation(
-    presentationId: string,
-    targetFolderId: string,
-    linksToCopy?: import("/Users/thinkle/BackedUpProjects/gas/gather-the-docs/src/gas/util/links").LinkToCopy[],
-    actionForUnknown?: "move" | "copy" | "ignore"
-  ): Promise<import("../gas/util/links").CopyResult[]> {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler(
-          (result: import("../gas/util/links").CopyResult[]) => resolve(result)
-        )
-        .withFailureHandler((error: any) => reject(error))
-        .copyLinksInPresentation(
-          presentationId,
-          targetFolderId,
-          linksToCopy,
-          actionForUnknown
-        );
-    });
-  },
+     onPickerCancel(): Promise<void> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: void) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .onPickerCancel();
+      });
+    },
 
-  getActivePresentation(): Promise<
-    import("../gas/slides/slidesAddOn").Document
-  > {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler(
-          (result: import("../gas/slides/slidesAddOn").Document) =>
-            resolve(result)
-        )
-        .withFailureHandler((error: any) => reject(error))
-        .getActivePresentation();
-    });
-  },
+     onPickerComplete(id: string): Promise<void> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: void) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .onPickerComplete(id);
+      });
+    },
 
-  getActiveUserEmail(): Promise<string> {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler((result: string) => resolve(result))
-        .withFailureHandler((error: any) => reject(error))
-        .getActiveUserEmail();
-    });
-  },
+     pickFolder(filetype?: "folder", title?: string): Promise<void> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: void) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .pickFolder(filetype, title);
+      });
+    },
 
-  getFunctionStatus(
-    fname: string
-  ): Promise<
-    import("../../../gas-long-process-poller/dist/status").ProcessUpdate
-  > {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler(
-          (
-            result: import("../../../gas-long-process-poller/dist/status").ProcessUpdate
-          ) => resolve(result)
-        )
-        .withFailureHandler((error: any) => reject(error))
-        .getFunctionStatus(fname);
-    });
-  },
+     getFolderInfo(folderId: string): Promise<import("../gas/copier").Folder> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: import("../gas/copier").Folder) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getFolderInfo(folderId);
+      });
+    },
 
-  getAddOnEnvironment(): Promise<"Slides" | "Docs" | "Sheets" | "Unknown"> {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler(
-          (result: "Slides" | "Docs" | "Sheets" | "Unknown") => resolve(result)
-        )
-        .withFailureHandler((error: any) => reject(error))
-        .getAddOnEnvironment();
-    });
-  },
+     harvestLinksFromActivePresentation(): Promise<import("../gas/util/links").DriveLink[]> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: import("../gas/util/links").DriveLink[]) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .harvestLinksFromActivePresentation();
+      });
+    },
 
-  createFolderForDocument(
-    id: string,
-    name: string,
-    parentId?: string
-  ): Promise<import("../gas/copier").Folder> {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler((result: import("../gas/copier").Folder) =>
-          resolve(result)
-        )
-        .withFailureHandler((error: any) => reject(error))
-        .createFolderForDocument(id, name, parentId);
-    });
-  },
+     copyLinksInPresentation(presentationId: string, targetFolderId: string, linksToCopy?: import("/Users/thinkle/BackedUpProjects/gas/gather-the-docs/src/gas/util/links").LinkToCopy[], actionForUnknown?: "move" | "copy" | "ignore"): Promise<import("../gas/util/links").CopyResult[]> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: import("../gas/util/links").CopyResult[]) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .copyLinksInPresentation(presentationId, targetFolderId, linksToCopy, actionForUnknown);
+      });
+    },
 
-  getFolderForDocument(id: string): Promise<import("../gas/copier").Folder> {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler((result: import("../gas/copier").Folder) =>
-          resolve(result)
-        )
-        .withFailureHandler((error: any) => reject(error))
-        .getFolderForDocument(id);
-    });
-  },
+     getActivePresentation(): Promise<import("../gas/slides/slidesAddOn").Document> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: import("../gas/slides/slidesAddOn").Document) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getActivePresentation();
+      });
+    },
 
-  setFolderForDocument(id: any, folderId: any): Promise<void> {
-    return new Promise((resolve, reject) => {
-      google.script.run
-        .withSuccessHandler((result: void) => resolve(result))
-        .withFailureHandler((error: any) => reject(error))
-        .setFolderForDocument(id, folderId);
-    });
-  },
-};
+     getActiveUserEmail(): Promise<string> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: string) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getActiveUserEmail();
+      });
+    },
+
+     getFunctionStatus(fname: string): Promise<import("../../../gas-long-process-poller/dist/status").ProcessUpdate> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: import("../../../gas-long-process-poller/dist/status").ProcessUpdate) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getFunctionStatus(fname);
+      });
+    },
+
+     getAddOnEnvironment(): Promise<"Slides" | "Docs" | "Sheets" | "Unknown"> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: "Slides" | "Docs" | "Sheets" | "Unknown") => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getAddOnEnvironment();
+      });
+    },
+
+     createFolderForDocument(id: string, name: string, parentId?: string): Promise<import("../gas/copier").Folder> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: import("../gas/copier").Folder) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .createFolderForDocument(id, name, parentId);
+      });
+    },
+
+     getFolderForDocument(id: string): Promise<import("../gas/copier").Folder> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: import("../gas/copier").Folder) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getFolderForDocument(id);
+      });
+    },
+
+     setFolderForDocument(id: any, folderId: any): Promise<void> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: void) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .setFolderForDocument(id, folderId);
+      });
+    }
+}
